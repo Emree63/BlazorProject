eb5977b570ac0496531c3f416b8d7084ae473d11
clement.laporte <clement.laporte@etu.uca.fr>
2022-12-10 09:50:55 +0100

    finish craftlayout according to initial model

--- src/CraftSharp/Shared/HeaderLayout.razor.cs
+++ src/CraftSharp/Shared/HeaderLayout.razor.cs
@@ -7,6 +7,6 @@ namespace CraftSharp.Shared
     public partial class HeaderLayout
     {
         [Inject]
-        public IStringLocalizer<Index> Localizer { get; set; }
+        public IStringLocalizer<HeaderLayout> Localizer { get; set; }
     }
 }
