871816f19266e77012d0731c66a5983da3d89f20
arthur.valin <arthur.valin@etu.uca.fr>
2022-12-31 17:19:50 +0100

    Adding error handler

---
+++ src/CraftSharp/App.razor.cs
@@ -0,0 +1,26 @@
+﻿using CraftSharp.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace CraftSharp
+{
+    public partial class App
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public ILogger<ErrorLayout> Logger { get; set; }
+
+        public async Task ErrorHandler(int countdown) {
+            Logger.Log(LogLevel.Error, $"Navigating to unknown page : {NavigationManager.Uri}");
+            while(countdown > 0)
+            {
+                await Task.Delay(1000);
+                countdown--;
+                StateHasChanged();
+            }
+            NavigationManager.NavigateTo("index");
+        }
+
+    }
+}
