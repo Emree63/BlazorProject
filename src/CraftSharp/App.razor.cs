d7a487adab02eb52a78b561d62a2b0e06a7c1cbd
arthur.valin <arthur.valin@etu.uca.fr>
2022-12-31 21:25:24 +0100

    Adding localization to error view

--- src/CraftSharp/App.razor.cs
+++ src/CraftSharp/App.razor.cs
@@ -1,9 +1,31 @@
 ﻿using CraftSharp.Shared;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 
 namespace CraftSharp
 {
     public partial class App
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public ILogger<ErrorLayout> Logger { get; set; }
+
+        [Inject]
+        public IStringLocalizer<App> Localizer { get; set; }
+
+        public async Task ErrorHandler(int countdown)
+        {
+            Logger.Log(LogLevel.Error, $"Navigating to unknown page : {NavigationManager.Uri}");
+            while (countdown > 0)
+            {
+                await Task.Delay(1000);
+                countdown--;
+            }
+            countdown = 5;
+            NavigationManager.NavigateTo("index");
+        }
+
     }
 }
